8be601274114b647773306d852663d39fcf442ba
hakon.isaksen <hakon.isaksen@gmail.com>
2022-10-05 12:10:03 +0200

    wip 2

--- Generator.py
+++ Generator.py
@@ -9,7 +9,11 @@ t_i = [0.1, 0.15, 0.1, 0.1, 0.15, 0.1, 0.2] #picking time
 u_i = [60, 36, 42, 42, 30, 60, 90]  #refilltime
 sections = [simpy.Container(env, 100, init =100 ), simpy.Container(env, 150, init =150 ), simpy.Container(env, 50, init =50 ), simpy.Container(env, 150, init =150 ), simpy.Container(env, 80, init =80 ), simpy.Container(env, 40, init =40 ), simpy.Container(env, 250, init =250 ) ]
 Counters = simpy.Resource(env, 4)
-time_in_queue = {"C1": [], "C2": [], "C3": [], "C4": []}
+Sections = simpy.resource(env,6)
+simTime = 16*60
+customer = 0
+mosList = []
+
 
 n_employees = 10
 p_i = 0.05 * n_employees    #number of employees actually working, threshold value
@@ -47,9 +51,13 @@ def generateMos(v, T_q):
 
 #as long as true, create new customer object with interval 1/lamda_c
 def customerGenerator(env):
+    #customer = 0
     while True:
+        customer +=1
         customer = Customer(env)
         yield env.timeout(np.random.exponential(1/lamda_c)) #maybe poison
+
+    
 
 #time it takes to walk inbetween sections       
 def T_t():
@@ -59,7 +67,8 @@ def T_t():
 class Customer(object):
     def _init_(self,env):
         self.omega = generateShoppingList()
-        self.i = 0
+        total_items = sum(self.omega)
+        self.i = 0  
         self.items = 0
         while self.i < 7:
             if self.omega[self.i]>0:
@@ -68,54 +77,49 @@ class Customer(object):
                     yield self.timeout(self.omega[self.i]*t_i[self.i])
                     self.items+= self.omega[self.i]
                     yield self.timeout(T_t())
-                    self.i+=1
+                    self.i+=1      
         
-        
-        
-
-                        
-        while self.items > 0:
-            #implement aquire 
             with Counters.request() as req:
                 queue_start = self.env.now
                 yield req
                 timestamp_2= self.env.now
                 T_q = timestamp_2-queue_start
-                yield self.timeout(t_7s*self.items)
-                yield self.timeout(t_7p)
+                if self.items > 0:
+                    yield self.timeout(t_7s*self.items)
+                    yield self.timeout(t_7p)
+
+        v = self.items/total_items
+        mosList.append(generateMos(v,T_q))      
                 
 
-                
-            #implement timer here, does not need to release
-            #excerise 5 
 
-            
-            
-            #implement release cashier
-
-
-
-        #implement: hvis kunde ikke har handlet noe self.items==0
-        #if self.items != 0:    -->     kan ta denne over while-loopen slik at hvis items=0, hopper den over dette
 
 
 class Employee(object):
     def _init_(self,env):
         self.i = 0
         while self.i < 7:
-            yield self.timeout(T_t()) 
-            if sections[self.i].level < sections[self.i].capacity * p_i:
-                yield sections[self.i].put(sections[self.i].capacity - sections[self.i].level)
-                yield self.timeout(u_i[self.i])
-            self.i += 1
-            if self.i >= 7:
-                self.i = 0
+            yield self.timeout(T_t())
+            with Sections.request() as req:
+                yield req
+                if sections[self.i].level < sections[self.i].capacity * p_i:
+                    yield sections[self.i].put(sections[self.i].capacity - sections[self.i].level)
+                    yield self.timeout(u_i[self.i])
+                self.i += 1
+                if self.i >= 7:
+                    self.i = 0
 
 
 
 generateShoppingList()
 
 
+
+env.process(customerGenerator(env))
+
+for y in range(1, n_employees):
+    env.process(employee = Employee(env))
+env.run(until=simTime)
 
 
 

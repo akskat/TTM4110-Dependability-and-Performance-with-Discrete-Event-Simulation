a287fba8308e2d9a133a5296176aee070d63d78a
akskat <aksel.nk@hotmail.no>
2022-10-05 16:08:39 +0200

    fixed functions

--- Generator.py
+++ Generator.py
@@ -11,11 +11,10 @@ sections = [simpy.Container(env, 100, init =100 ), simpy.Container(env, 150, ini
 Counters = simpy.Resource(env, 4)
 Sections = simpy.Resource(env,6)
 simTime = 16*60
-customer = 0
 mosList = []
 
 
-n_employees = 10
+n_employees = 1
 p_i = 0.05 * n_employees    #number of employees actually working, threshold value
 lamda_c = 1/3
 lamda_t = 2
@@ -49,75 +48,73 @@ def generateMos(v, T_q):
     else:
         return 1
 
+
 #as long as true, create new customer object with interval 1/lamda_c
 def customerGenerator(env):
-    #customer = 0
     while True:
-        customer = Customer(env)
+        new_customer = Customer(env)
+        env.process(new_customer)
         yield env.timeout(np.random.exponential(1/lamda_c)) #maybe poison
 
-    
+
 
 #time it takes to walk inbetween sections       
 def T_t():
     return np.random.exponential(1/lamda_t)
 
 
-class Customer(object):
-    def _init_(self,env):
-        self.omega = generateShoppingList()
-        total_items = sum(self.omega)
-        self.i = 0  
-        self.items = 0
-        while self.i < 7:
-            if self.omega[self.i]>0:
-                if self.omega[self.i]<= sections[self.i].level: 
-                    yield sections[self.i].get(self.omega[self.i])
-                    yield self.timeout(self.omega[self.i]*t_i[self.i])
-                    self.items+= self.omega[self.i]
-                    yield self.timeout(T_t())
-                    self.i+=1      
-        
-            with Counters.request() as req:
-                queue_start = self.env.now
-                yield req
-                timestamp_2= self.env.now
-                T_q = timestamp_2-queue_start
-                if self.items > 0:
-                    yield self.timeout(t_7s*self.items)
-                    yield self.timeout(t_7p)
-
-        v = self.items/total_items
-        mosList.append(generateMos(v,T_q))      
+def Customer(env):
+    omega = generateShoppingList()
+    total_items = sum(omega)
+    i = 0  
+    items = 0
+    while i < 7:
+        if omega[i] > 0:
+            if omega[i] <= sections[i].level: 
+                yield sections[i].get(omega[i])
+                yield env.timeout(omega[i]*t_i[i])
+                items += omega[i]
                 
+        yield env.timeout(T_t())
+        i += 1 
+    
+    with Counters.request() as req:
+        queue_start = env.now
+        yield req
+        timestamp_2 = env.now
+        T_q = timestamp_2-queue_start
+        if items > 0:
+            yield env.timeout(t_7s*items)
+            yield env.timeout(t_7p)
+
+    v = items/total_items
+    mosList.append(generateMos(v,T_q))
+    
+    print(sum(mosList)/len(mosList))            
 
 
 
 
-class Employee(object):
-    def _init_(self,env):
-        self.i = 0
-        while self.i < 7:
-            yield self.timeout(T_t())
-            with Sections.request() as req:
-                yield req
-                if sections[self.i].level < sections[self.i].capacity * p_i:
-                    yield sections[self.i].put(sections[self.i].capacity - sections[self.i].level)
-                    yield self.timeout(u_i[self.i])
-                self.i += 1
-                if self.i >= 7:
-                    self.i = 0
+def Employee(env):
+    i = 0
+    while i < 7:
+        yield env.timeout(T_t())
+        with Sections.request() as req:
+            yield req
+            if sections[i].level < sections[i].capacity * p_i:
+                yield sections[i].put(sections[i].capacity - sections[i].level)
+                yield env.timeout(u_i[i])
+            i += 1
+            if i >= 7:
+                i = 0
 
-
-
-generateShoppingList()
 
 
 
 env.process(customerGenerator(env))
-
 for y in range(1, n_employees):
-    env.process(employee = Employee(env))
+    env.process(Employee(env))
+
 env.run(until=simTime)
 
 

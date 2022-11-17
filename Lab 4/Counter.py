24c02f54384433d31ae0cc4e59c6eb830f6d91c1
akskat <aksel.nk@hotmail.no>
2022-11-17 16:42:41 +0100

    finpuss b3

--- Lab 4/Counter.py
+++ Lab 4/Counter.py
@@ -6,39 +6,16 @@ import matplotlib.pyplot as plt
 
 
 env = simpy.Environment()
-N_i = [100,150,50,150,80,40,250] #maximum capacity 
-t_i = [0.1, 0.15, 0.1, 0.1, 0.15, 0.1, 0.2] #picking time
-u_i = [60, 36, 42, 42, 30, 60, 90]  #refilltime
-sections = [simpy.Container(env, 100, init =100 ), simpy.Container(env, 150, init =150 ), simpy.Container(env, 50, init =50 ), simpy.Container(env, 150, init =150 ), simpy.Container(env, 80, init =80 ), simpy.Container(env, 40, init =40 ), simpy.Container(env, 250, init =250 ) ]
-employeeResources = [simpy.Resource(env,1),simpy.Resource(env,1),simpy.Resource(env,1),simpy.Resource(env,1),simpy.Resource(env,1),simpy.Resource(env,1),simpy.Resource(env,1)]
 Counters = simpy.PreemptiveResource(env, 4) #er dette riktig? Var før kun Resource
-simTime = 16*60
-mosList = []
-mosListAvg = []
-nrEmployees = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-n_employees = 1
-p_i = 0.05 * n_employees    #number of employees actually working, threshold value
+simTime = 500000 #number of employees actually working, threshold value
 lamda_c = 1/3
-lambda_failure = 1/(4*60)
+lambda_failure = 4/(4*60)
 lambda_repair = 1/(15)
 lambda_T_co = 1/2
-lamda_t = 2
-N_7 = 4
-t_7s = 0.1
-t_7p = 0.2
 Q_time = []
-
-#create random shopping list
-def generateShoppingList():
-    omega = []
-    for i in range(0,7):
-        x = random.randint(0,5)
-        omega.append(x)
-    while sum(omega) == 0:
-        for i in range(0,7):
-            x = random.randint(0,5)
-            omega.append(x)
-    return omega
+availability_counter = []
+down_time = 0
+down_time_list = []
 
 
 
@@ -56,30 +33,7 @@ def failureGenerator(env):
         yield env.timeout(np.random.exponential(1/lambda_failure)) 
 
 
-
-#time it takes to walk inbetween sections       
-def T_t():
-    return np.random.exponential(1/lamda_t)
-
-
 def Customer(env):
-    omega = generateShoppingList()
-    total_items2 = sum(omega)
-    i = 0  
-    items = 0
-    items2 = 0
-    while i < 7:
-        yield env.timeout(T_t())
-        if omega[i] > 0:
-            if omega[i] <= sections[i].level: 
-                yield sections[i].get(omega[i])
-                yield env.timeout(omega[i]*t_i[i])
-                items2 += omega[i]
-                if i == 0:
-                    items += 1
-                elif i != 0:
-                    items += omega[i]
-        i += 1 
     with Counters.request() as req:
         queue_start = env.now
         yield req
@@ -89,39 +43,33 @@ def Customer(env):
         yield env.timeout(np.random.exponential(1/lambda_T_co))
     
 
-
-
 def Failure(env):
-
+    availability_counter.append(1)
+    T_A1 = 0
+    T_A2 = 0
+    if (len(availability_counter) > 3):
+        T_A1 = env.now
     with Counters.request(priority=0, preempt=True) as req:
         yield req
         yield env.timeout(np.random.exponential(1/lambda_repair)) 
+    del availability_counter[0]
+    if (len(availability_counter) >= 3):
+        T_A2 = env.now
+    T_A_total = T_A2 - T_A1
+    down_time_list.append(T_A_total)
 
-
-
-
-def Employee(env):
-    i = 0
-    while i < 7:
-        yield env.timeout(T_t())            
-        if employeeResources[i].count < 1:
-            with employeeResources[i].request() as req:
-                yield req
-                if sections[i].level < sections[i].capacity * p_i:
-                    yield env.timeout(u_i[i])
-                    yield sections[i].put(sections[i].capacity - sections[i].level)
-                    #yield env.timeout(u_i[i])
-        i += 1
-        if i >= 7:
-            i = 0
 
 env.process(customerGenerator(env))
 env.process(failureGenerator(env))
-for y in range(1, n_employees):
-    env.process(Employee(env))
-
 
 env.run(until=simTime)
 
-print(Q_time)
+#print(Q_time)
+
+avg_waiting_times = sum(Q_time)/len(Q_time)
+print("Average: ", avg_waiting_times)
+down_time = sum(down_time_list)
+availability = (simTime-down_time)/simTime
+print("Availability: ", availability)
+
 
